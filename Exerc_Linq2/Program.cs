2f775962448fc351d492238719dc7315622dd995
Fabrício Oliveira Dias <fabricio.dias102@gmail.com>
2021-05-16 12:29:48 -0300

    Exercicio 2 referente a linq, ler dados de um funcionario a partir de um arquivo .csv

---
+++ Exerc_Linq2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Exerc_Linq2.Services;
+using System.Linq;
+using System.IO;
+using System.Globalization;
+
+namespace Exerc_Linq2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+            Console.Write("Enter to Salary: ");
+            double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            List<Employee> list = new List<Employee>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fields = sr.ReadLine().Split(',');
+                        string name = fields[0];
+                        string email = fields[1];
+                        double salary = double.Parse(fields[2], CultureInfo.InvariantCulture);
+                        list.Add(new Employee(name, email, salary));
+                    }
+                }
+
+                var emails = list.Where(obj => obj.Salary > limit).OrderBy(obj => obj.Email).Select(obj => obj.Email);
+                            
+                var salarys = list.Where(obj => obj.Name[0] == 'M').Sum(obj => obj.Salary);
+
+                Console.WriteLine("Email of people whose salary is more than " + limit.ToString("F2", CultureInfo.InvariantCulture));
+                foreach (string email in emails)
+                {
+                    Console.WriteLine(email);
+                }
+
+                Console.WriteLine("Sum of salary of people whose name starts with 'M': " + salarys.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("ERROR occured");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Exerc_Linq2/Services/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerc_Linq2.Services
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+    }
+}
